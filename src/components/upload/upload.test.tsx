471eeaf9d76a1b096dc190f0b6330616154a9c6d
杨国平 <756517553@qq.com>
2023-03-17 13:56:22 +0800

    feat:fix

--- src/components/upload/upload.test.tsx
+++ src/components/upload/upload.test.tsx
@@ -1,11 +1,8 @@
-
-
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Upload from './upload';
 
 describe("Upload Test:", () => {
-
 	test("Test 1: test upload:", () => {
 		const { getByTitle } = render(<Upload url='robin' name="robin" onClick={() => { console.log("robin") }} />)
 		var div = getByTitle('robin')
