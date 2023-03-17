e07e1b28a8657e6ec3164ac1a8969909faf43332
杨国平 <756517553@qq.com>
2023-03-17 14:06:16 +0800

    feat:add table components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { default as Upload } from './upload/upload';
 export { default as Modal } from './modal/modal';
 export { default as Tab } from './tab/tab';
 export { default as Flex } from './flex/flex';
-export { default as FlexItem} from './flex_item/flex_item';
+export { default as FlexItem } from './flex_item/flex_item';
+export { default as Table } from './table/table';
---
+++ src/components/table/table.tsx
@@ -0,0 +1,52 @@
+
+import React, { ReactElement, useMemo } from 'react';
+
+interface IColProps {
+	key: string;
+	title: string;
+	render?(data: any): ReactElement
+}
+
+interface ITableProps {
+	className?: string;
+	data: Array<any>;
+	cols: Array<IColProps>
+}
+
+export default function Table(props: ITableProps) {
+	const { data, cols, className } = props;
+
+	const col = useMemo(() => {
+		return cols?.map(c => {
+			return <td key={c?.key}>
+				{c?.title}
+			</td>
+		})
+	}, [cols])
+
+	return <table className={className}>
+		<thead>
+			<tr>{col}</tr>
+		</thead>
+		<tbody>
+			{
+				data?.map(d => {
+					return <tr key={d.id}>
+						{cols?.map(c => {
+							if (c.render) {
+								return <td key={c.key}>
+									{
+										c.render(d)
+									}
+								</td>
+							}
+							return <td key={c.key}>
+								{d[c.key]}
+							</td>
+						})}
+					</tr>
+				})
+			}
+		</tbody>
+	</table>
+}
