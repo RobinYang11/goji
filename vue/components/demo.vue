2723f24a2a816efca1ac19fd9d0e46e374ca4252
杨国平 <756517553@qq.com>
2023-03-22 13:53:18 +0800

    :memo: add vue demo

---
+++ vue/components/demo.vue
@@ -0,0 +1,16 @@
+
+<template>
+	 <div class="demo">this is a {{name}}</div>	
+</template>
+<style scoped>
+	.demo{
+		color:red
+	}
+</style>
+<script>
+export default {
+	data() {
+		name:'Demo'
+	}
+}
+</script>
