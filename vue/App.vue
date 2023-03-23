ac2fc9587b969d71fc1730fdd5da573775562ef5
ebenchen <2507988376@qq.com>
2023-03-23 15:50:11 +0800

    add: 初版Ebtable组件-基于原生table

--- vue/App.vue
+++ vue/App.vue
@@ -1,19 +1,85 @@
 
 <template>
   <div class="App">
-    <Demo/>
+    <EbTable
+        index
+        :data="data"
+        :col="col"
+        align="right"
+    />
   </div>
 </template>
 
 <script>
-import Demo from './components/demo.vue';
+import EbTable from './components/Table/index.vue';
 export default {
-  components: { Demo },
+  components: { EbTable },
   name: 'App',
 
   data() {
     return {
         msg: "有追求的",
+      testCheck: true,
+      data: [
+        {
+          col1: 'data1',
+          col2: 'data2',
+          col3: 'data3',
+          col4: 'data4',
+          col5: 'data5',
+          col6: 'data6'
+        },
+        {
+          col4: 'data4',
+          col5: 'data5',
+          col6: 'data6'
+        }
+      ],
+      col: [
+        {
+          name: 'col1',
+          label: '列1',
+          width: '200px'
+        },
+        {
+          name: 'col2',
+          label: '列2',
+          align: 'left'
+        },
+        {
+          name: 'col3',
+          label: '列3'
+        },
+        {
+          name: 'col4',
+          label: '列4',
+          align: 'right'
+        },
+        {
+          name: 'col5',
+          label: '列5'
+        },
+        {
+          name: 'col6',
+          label: '列6'
+        },
+        {
+          name: 'col7',
+          label: '列7'
+        },
+        {
+          name: 'col8',
+          label: '列8'
+        },
+        {
+          name: 'col9',
+          label: '列9'
+        },
+        {
+          name: 'col10',
+          label: '列10'
+        }
+      ]
     };
   }
 };
