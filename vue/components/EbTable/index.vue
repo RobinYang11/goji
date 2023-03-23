16a5e291a3f9d120264be9ad2d4448345521b570
ebenchen <2507988376@qq.com>
2023-03-23 23:14:59 +0800

    add: 初版EbTable组件-基于初始化数据进行筛选

--- vue/components/EbTable/index.vue
+++ vue/components/EbTable/index.vue
@@ -2,12 +2,12 @@
   <div v-if="col.length" class="eb-table eb-table-thr-border" :style="{ width: width ? width : '100%'}">
     <table width="100%" cellspacing="0" cellpadding="0">
       <!-- 表头行 -->
-      <thead class="eb-table-head-bg">
+      <thead class="eb-table-head-bg" :style="styleConfig?.headerStyle">
         <th v-if="index" class="eb-table-thr-border eb-table-thr-index" align="center">序号</th>
         <th v-for="cItem in newCol" :width="cItem.width" :align="cItem.align ? cItem.align : 'center'" class="eb-table-thr-border">
           {{ cItem.label }}
           <!--    过滤-筛选     -->
-          <el-icon v-if="cItem.filter" class="eb-table-th-icon" @click="cItem.showFilter = !cItem.showFilter"><Search /></el-icon>
+          <el-icon v-if="cItem.filter" class="eb-table-th-icon" @click="() => { cItem.showFilter = !cItem.showFilter; this.filterContent = ''}"><Search /></el-icon>
           <div v-if="cItem.filter && cItem.showFilter" class="eb-table-th-filterbox" @mouseleave="cItem.showFilter = false">
             <input :value="filterContent" placeholder="请输入过滤内容" class="eb-table-th-filterbox-input" @change="handleFilterContent"/>
             <input value="搜索" type="button" class="eb-table-th-filterbox-button" @click="handleFilter(cItem.name)" />
@@ -67,6 +67,7 @@ export default {
     align: String,
     selection: Boolean,
     index: Boolean,
+    styleConfig: Object,
   },
   data() {
     return {
@@ -98,9 +99,7 @@ export default {
     },
     // 处理过滤内容
     handleFilter(colKey) {
-      // console.log(colKey)
-      // console.log(this.filterContent)
-      // console.log(this.data)
+      this.newData = this.data.filter(item => item[colKey] && item[colKey].indexOf(this.filterContent) !== -1)
     },
     // 排序--降序
     handleSortDown(cItem, key) {
@@ -202,13 +201,34 @@ export default {
 .eb-table-th-filterbox {
   position: absolute;
   z-index: 999;
-  width: 150px;
-  height: 60px;
-  background-color: antiquewhite;
+  width: 180px;
+  height: 70px;
   left: -150px;
-  top: 0;
+  top: -10px;
   padding: 20px 5px;
+  border-radius: 4px;
+  border: 1px solid #305ee8;
+  background-color: #b8f2f9;
+  /*box-shadow:  5px 5px 10px #183076,*/
+  /*-5px -5px 10px #488cff;*/
 }
-.eb-table-th-filterbox-input { display: inline-block; width: 132px; height: 28px; margin-bottom: 8px }
-.eb-table-th-filterbox-button { display: inline-block; width: 140px; height: 28px; }
+.eb-table-th-filterbox-input {
+  display: inline-block; width: 138px; height: 26px; margin-bottom: 8px;
+  outline: none;
+  border: none;
+}
+.eb-table-th-filterbox-input:focus{
+  border: 1px solid #305ee8;
+}
+.eb-table-th-filterbox-button {
+  display: inline-block; width: 140px; height: 28px;
+  background-color: rgba(48,94,232, 0.6);
+  color: #fff;
+  outline: none;
+  border: none;
+  cursor: pointer;
+}
+.eb-table-th-filterbox-button:active {
+  background-color: rgba(48,94,232, 1);
+}
 </style>
