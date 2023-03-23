e515da27c14d2897d2f5acab028a5bc5d873559a
Davdeyang <1320501826@qq.com>
2023-03-23 18:01:51 +0800

    feat:dropDown组件

--- src/_test.tsx
+++ src/_test.tsx
@@ -1,19 +1,19 @@
-
-import React, { useState } from 'react';
-import ReactDom from 'react-dom'
-import Modal from './components/modal/modal';
-import Upload from './components/upload/upload';
-import Tab from './components/tab/tab';
-import * as GOJI from 'goji_ui'
-import DropDown from './components/dropDown';
+import React, { useState } from "react";
+import ReactDom from "react-dom";
+import Modal from "./components/modal/modal";
+import Upload from "./components/upload/upload";
+import Tab from "./components/tab/tab";
+import * as GOJI from "goji_ui";
+import DropDown from "./components/dropDown";
 
 function App() {
-	const [visible, setVisible] = useState(false)
-	const [ev, setEv] = useState(false)
-	const [modalVisible,setModalVisible]= useState(false);
+  const [visible, setVisible] = useState(false);
+  const [ev, setEv] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
-	return <div>
-		{/* <GOJI.Tab
+  return (
+    <div>
+      {/* <GOJI.Tab
 			extSelector={'.tab'}
 			extension={<div>这是扩展的内容</div>}
 			items={[
@@ -30,96 +30,129 @@ function App() {
 			]}
 		/> */}
 
+      <h1>test</h1>
+      <button
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        show modal
+      </button>
+      <Tab
+        onTabChange={() => {
+          setEv(true);
+        }}
+        hiddenStyle={{
+          height: "0px",
+          overflow: "hidden",
+        }}
+        tabContentVisible={ev}
+        extSelector={'[aria-label="tab"]'}
+        extension={
+          <div
+            onClick={() => {
+              setEv(!ev);
+            }}
+            className="ext"
+          >
+            这是扩展的内容
+          </div>
+        }
+        items={[
+          {
+            title: "tab1",
+            key: "tab1",
+            children: <div>tab1</div>,
+          },
+          {
+            title: "tab2",
+            key: "tab2",
+            children: <div>tab2</div>,
+          },
+        ]}
+      />
 
-		<h1>test</h1>
-		<button
-			onClick={() => {
-				setVisible(true)
-			}}
-		>
-			show modal
-		</button>
-		<Tab
-			onTabChange={()=>{
-				setEv(true)
-			}}
-			hiddenStyle={{
-				height:'0px',
-				overflow:'hidden'
-			}}
-			tabContentVisible={ev}
-			extSelector={'[aria-label="tab"]'}
-			extension={<div onClick={() => { setEv(!ev) }} className="ext">这是扩展的内容</div>}
-			items={[
-				{
-					title: "tab1",
-					key: "tab1",
-					children: <div>tab1</div>
-				},
-				{
-					title: "tab2",
-					key: "tab2",
-					children: <div>tab2</div>
-				}
-			]}
-		/>
+      <Modal
+        // dontDestroyOnClose={true}
+        onClose={() => {
+          setVisible(false);
+        }}
+        visible={visible}
+      >
+        <div>
+          how to set default value for typescript interface field
+          <input type="text" />
+        </div>
+      </Modal>
 
-		<Modal
-			// dontDestroyOnClose={true}
-			onClose={() => {
-				setVisible(false)
-			}}
-			visible={visible}
-		>
-			<div>
-				how to set default value for typescript interface field
-				<input type="text" />
-			</div>
-		</Modal>
+      <Upload
+        uploadUrl="/api/video-service/upload"
+        beforeUpload={(f) => {
+          for (var i = 0; i < f.length; i++) {
+            console.log(f[i].name);
+          }
+          return new Promise((r, j) => {
+            setTimeout(() => {
+              r(f);
+            }, 1000);
+          });
+        }}
+        valueFilter={({ response }) => {
+          return (response as Record<string, Object>).url;
+        }}
+        onComplete={(res: any[]) => {
+          console.log(res);
+        }}
+      >
+        请选择文件
+      </Upload>
 
-		<Upload
-			uploadUrl='/api/video-service/upload'
-			beforeUpload={(f) => {
-				for (var i = 0; i < f.length; i++) {
-					console.log(f[i].name)
-				}
-				return new Promise((r, j) => {
-					setTimeout(() => {
-						r(f)
-					}, 1000);
-				})
-			}}
-
-			valueFilter={({ response }) => {
-				return (response as Record<string, Object>).url
-			}}
-			onComplete={(res: any[]) => {
-				console.log(res)
-			}}
-		>
-			请选择文件
-		</Upload>
-
-
-		<DropDown
-			trigger={'click'}
-			visible={modalVisible}
-			onChangeVisible={()=>{
-				setModalVisible(!modalVisible)
-			}}
-			width={100}
-			disable={false}
-			style={{
-				width:"100px",
-				height:"100px",
-				backgroundColor:"green"
-			}}
-			modelContent={<div>弹框内容</div>}
-			// className={"modless"}
-		>
-			SHOW MODEL
-		</DropDown>
-	</div >
+      <DropDown
+        trigger={"hover"}
+        visible={modalVisible}
+        onChangeVisible={() => {
+          setModalVisible(!modalVisible);
+        }}
+        // width={100}
+        disable={false}
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: "green",
+        }}
+        modelContent={[
+          {
+            id: "1",
+            label: (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="#"
+              >
+                1st menu item
+              </a>
+            ),
+          },
+          {
+            id: "2",
+            label: (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="#"
+              >
+                2nd menu item
+              </a>
+            ),
+          },
+        ]
+			}
+        // className={"modless"}
+      >
+        SHOW MODEL <img src="../static/下箭头.jpg"/>
+      </DropDown>
+    </div>
+  );
 }
 
-ReactDom.render(<App />, document.getElementById("app"))
+ReactDom.render(<App />, document.getElementById("app"));
--- src/components/dropDown/index.tsx
+++ src/components/dropDown/index.tsx
@@ -3,7 +3,7 @@ import { createPortal } from "react-dom";
 import styles from "./dropDown.module.less";
 
 type trigger = "click" | "hover";
-
+type position = "left" | "right" | "top" | "bottom";
 interface IDropDown {
   // trigger?: ("click" | "hover")[];
   trigger: trigger;
@@ -12,8 +12,9 @@ interface IDropDown {
   children?: ReactNode;
   visible?: boolean;
   disable?: boolean;
-  modelContent: ReactNode;
-  width: number;
+  modelContent: object[];
+  width?: number;
+  position?: position;
   onChangeVisible: () => void;
 }
 
@@ -26,10 +27,38 @@ const DropDown: React.FC<IDropDown> = ({
   visible,
   disable,
   modelContent,
+  position,
   onChangeVisible,
 }) => {
   const [show, setShow] = useState(visible);
-  const dropDown = document.getElementById('dropDown') as HTMLElement;
+  const dropDown = document.getElementById("dropDown") as HTMLElement;
+
+  const fixedPositon = () => {
+    switch (position) {
+      case "left":
+        return {
+          bottom: 0,
+          left: 0,
+        };
+      case "right":
+        return {
+          right: 0,
+          top: 0,
+        };
+      case "top":
+        return {
+          bottom: 0,
+          left: 0,
+        };
+      case "bottom":
+        return {
+          bottom: 0,
+          left: 0,
+        };
+      default:
+        break;
+    }
+  };
 
   return (
     <div id="dropDown" className={styles.dropDown}>
@@ -54,8 +83,22 @@ const DropDown: React.FC<IDropDown> = ({
 
       {(show === true || visible === true) &&
         createPortal(
-          <div className={styles.model} style={{ width: `${width}px` }}>
-            {modelContent}
+          <div
+            className={styles.model}
+            style={{ width: `${width}px` }}
+            onMouseEnter={() => setShow(true)}
+            onMouseLeave={() => setShow(false)}
+          >
+            <ul className={styles.modelList}>
+              {modelContent?.map((item) => {
+                return (
+                  <li key={item?.id} className={styles.modalListItem}>
+                    {item?.label}
+                  </li>
+                );
+              })}
+            </ul>
+            {/* {modelContent} */}
           </div>,
           dropDown
         )}
