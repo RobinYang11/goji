3e59121c172f345c8f88263a2c1242c3ceab25d0
丁星星 <1693992774@qq.com>
2023-03-24 23:31:03 +0800

    feat：新增点击页面某个地方，出项浮框组件

--- vue/components/demo.vue
+++ vue/components/demo.vue
@@ -1,16 +1,49 @@
 
 <template>
-	 <div class="demo">this is a {{name}}</div>	
+  <div class="demo">
+    this is a {{ name }}
+    <div class="container" @click="showBubble">
+      <Bubble
+        v-if="isBubbleShow"
+        :content="bubbleContent"
+        :position="bubblePosition"
+      >
+      </Bubble>
+    </div>
+  </div>
 </template>
 <style scoped>
-	.demo{
-		color:red
-	}
+.demo {
+  color: red;
+}
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: #f7f7f7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
 <script>
+import Bubble from './Bubble.vue'
 export default {
-	data() {
-		name:'Demo'
-	}
+	components: {
+    Bubble
+  },
+  data () {
+		return {
+			name: 'Demo',
+			isBubbleShow: false,
+      bubbleContent: '点击页面任意位置',
+      bubblePosition: [0, 0]
+		}
+  },
+	methods: {
+    showBubble (event) {
+      this.isBubbleShow = !this.isBubbleShow
+      this.bubblePosition = [event.clientX, event.clientY]
+    }
+  }
 }
 </script>
